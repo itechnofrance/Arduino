fd70f02163dac0690fbb60138abd8cd29f042d8b
itechnofrance <36210638+itechnofrance@users.noreply.github.com>
2018-03-03 09:38:32 +0100

    Add files via upload

---
+++ librairies/bh1750/examples/luminosite_haute_resolution_continu_arduino/luminosite_haute_resolution_continu_arduino.ino
@@ -0,0 +1,26 @@
+/*
+ * Programme exemple pour lire la luminosité en continu en mode haute résolution 
+ * pour un Arduino Nano
+ */
+
+#include <Wire.h>  // ajout de librairie de gestion I2C
+#include <bh1750.h>  // ajout de la librairie de gestion du capteur BH1750
+
+bh1750 mesure_lumiere;  // création d'une instance
+
+void setup()
+{
+  Serial.begin(115200);
+  mesure_lumiere.begin(0x23);  // initialise la librairie BH1750 pour une utilisation Arduino Nano
+  // adresse 0x23 si broche ADD non branchée ou relié à 5v ou 3.3v
+  // adresse 0x5c si broche ADD branchée sur Ov
+}
+
+void loop() 
+{
+  uint16_t lux = mesure_lumiere.lecture_lumiere(MODE_CONTINU_HAUTE_RESOLUTION);  // effectue une mesure
+  Serial.print("Luminosité : ");
+  Serial.print(lux);
+  Serial.println(" Lux");
+  delay(1000);
+}
---
+++ librairies/bh1750/examples/luminosite_haute_resolution_une_mesure/luminosite_haute_resolution_une_mesure.ino
@@ -0,0 +1,30 @@
+/*
+ * Programme exemple pour lire la luminosité une fois en mode haute résolution 
+ * pour un NodeMCU Lolin ou Wemos D1 mini
+ */
+
+#include <Wire.h>  // ajout de librairie de gestion I2C
+#include <bh1750.h>  // ajout de la librairie de gestion du capteur BH1750
+
+bh1750 mesure_lumiere;  // création d'une instance
+
+// déclaration des broches utilisées sur Wemos D1 mini
+int sda = D2;  // GPIO4
+int scl = D1;  // GPIO5
+
+void setup()
+{
+  Serial.begin(115200);
+  mesure_lumiere.begin(0x23, sda, scl);  // initialise la librairie BH1750 pour une utilisation Wemos D1 mini
+  // adresse 0x23 si broche ADD non branchée ou relié à 5v ou 3.3v
+  // adresse 0x5c si broche ADD branchée sur Ov
+}
+
+void loop() 
+{
+  uint16_t lux = mesure_lumiere.lecture_lumiere(MODE_UNE_MESURE_HAUTE_RESOLUTION);  // effectue une mesure
+  Serial.print("Luminosité : ");
+  Serial.print(lux);
+  Serial.println(" Lux");
+  delay(1000);
+}
